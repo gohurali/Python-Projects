9496dd908aa60b7d16efa8711c24055e4a3523b8
gohurali <gohurali@live.com>
2018-11-06 20:18:46 -0800

    input validation:

--- WordStylizer/style.py
+++ WordStylizer/style.py
@@ -1,14 +1,19 @@
 word = input('Give me a word: ')
 spacer = input('How much do you want to space it by? ')
 
+try:
+    spacer = int(spacer)
 
-length_of_word = len(word)
-space_counter = 0
-i = 0
-for letter in word:
-    if(i>=1):
-        for space in range(space_counter):
-            print(' ',end='')
-    print(letter)
-    i+= 1
-    space_counter += int(spacer)
+    length_of_word = len(word)
+    space_counter = 0
+    i = 0
+    for letter in word:
+        if (i >= 1):
+            for space in range(space_counter):
+                print(' ', end='')
+        print(letter)
+        i += 1
+        space_counter += spacer
+except:
+    print('Must be a number.')
+
